383d5abf708b1411b26f6f6b33c0302146e21d39
Shreya9code <shreyadenre27@gmail.com>
2025-06-28 17:25:27 +0530

    InterviewHeader

--- app/interview/layout.jsx
+++ app/interview/layout.jsx
@@ -1,6 +1,6 @@
 "use client"
 import React, { useState } from 'react'
-import InterviewHeader from './_components/interviewHeader'
+import InterviewHeader from './_components/InterviewHeader'
 import { InterviewDataContext } from '@/context/InterviewDataContext'
 const InterviewLayout = ({children}) => {
   const [interviewInfo,setInterviewInfo] = useState(null);
